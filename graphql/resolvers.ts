c1beedc8fe539a09c11e3b319bce8f827a3ccf02
Luis Santiago Duque Franco <78662656+lduquef@users.noreply.github.com>
2021-11-18 19:58:21 -0500

    config back

---
+++ graphql/resolvers.ts
@@ -0,0 +1,11 @@
+import { userModel } from "../models/user";
+
+const resolvers ={
+    Query :{
+    Usuarios: async (parent ,args) => {
+        const usuarios = await userModel.find();
+        return usuarios;
+    }
+}
+}
+export {resolvers};
---
+++ graphql/types.ts
@@ -0,0 +1,29 @@
+import {gql} from 'apollo-server-express';
+import { Enum_estadoUsuario } from '../models/enums';
+
+const typeDefs = gql`
+    enum Enum_estadoUsuario {
+        PENDIENTE
+        AUTORIZADO 
+        NO_AUTRIZADO
+    }
+    enum Enum_Rol{
+    ESTUDIANTE
+    LIDER
+    ADMINISTRADOR 
+    }
+    type Usuario{
+        _id : ID!
+        nombre: String!
+        apellido:String!
+        correo : String!
+        estado : Enum_estadoUsuario!
+        rol : Enum_Rol
+    }
+
+    type Query {
+        Usarios:[usuario]
+    }
+    `;
+
+export{typeDefs};
