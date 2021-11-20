b31ab46fcc6096cd2420e7dd83bfa5cc16235f46
Luis Santiago Duque Franco <78662656+lduquef@users.noreply.github.com>
2021-11-19 20:06:50 -0500

    back

--- index.ts
+++ index.ts
@@ -1,103 +1,24 @@
-import  conectarBD from './db/db';
-import {userModel} from './models/user';
-import { projectModel } from './models/projects';
-import { avanceModel } from './models/avance';
-import { solicitudModel } from './models/solicitud';
-import { Enum_estadoUsuario } from './models/enums';
-solicitudModel
-const main = async ()=>{
-    await conectarBD(); 
+import express  from "express";
+import cors from 'cors';
+import {ApolloServer} from 'apollo-server-express';
+import dotenv from 'dotenv';
+import conectarBD from "./db/db";
+import {typeDefs} from './graphql/types'
+import { resolvers } from "./graphql/resolvers";
+dotenv.config();
 
-    await solicitudModel.create({
-        proyecto : "61933ef92624068a6c651dd4",
-        estudiante:"61932bc8ea438642ed930ab7",
-        estado: Enum_estadoUsuario.pendiente 
-    }).then((u)=> {
-        console.log("avance creado",u)
-    }).catch(e=>{
-        console.error("error creando el avance, verifique el correo",e)
-    })
+const server = new ApolloServer({
+    typeDefs : typeDefs,
+    resolvers : resolvers,
+})
 
-    const avance = await  avanceModel.find({ descripcion : "primer proyecto"});
-    console.log(avance[0].observaciones)
+const app = express();
+app.use(express.json);
+app.use(cors)
 
-}   
-
-
-main();
-
-
-// de usuario
-
-//crear Usuario
-    // await userModel.create({
-    //     nombre:"Edwer",
-    //     apellido:"llanos",
-    //     correo:"edw@c.com",
-    //     identificacion:"1005",
-    //     rol:Enum_rol.estudiante,
-
-    // }).then((u)=> {
-    //     console.log("usuario creado",u)
-    // }).catch(e=>{
-    //     console.error("error creando el usuario, verifique el correo",e)
-    // })
-    //obtener el get
-    // await userModel.find().then((u)=>{
-    //     console.log("usuarios",u)
-    // }).catch( e =>{
-    //     console.log("error usuario",e)
-    // }
-    // )
-
-    //busca el primero por el criterio y el 2 {} es la modificacion
-    //then , dado que funciona retorna ejecuta una funcion appenas la promesa termine
-    //catch captura el errror
-//Editar usuario
-    // await userModel.findOneAndUpdate({correo:'dsl@c.com'},{nombre:"juan"}).then((u)=>{
-    //     console.log("usuario actualizado",u)
-    // }).catch(e => {
-    //     console.log("eroor",e)
-    // })
-//eliminar usuario
-    // await userModel.findOneAndDelete({correo:"gg@.com"}).then((u)=>{
-    //     console.log("usuario eliminado",u)
-    // }).catch(e => {
-    //     console.log("eroor",e)
-    // })
-// // hacer busqueda usuario
-//     await userModel.findOne({correo:"gg@.com"}).then((u)=>{
-//         console.log("usuario encontrado",u)
-//     }).catch(e => {
-//         console.log("eroor",e)
-//     })
-
-//proyecto 
-    // projectModel.create({
-    //     nombre:"proyecto2",
-    //     presupuesto:124,
-    //     fechaInicio: Date.now(),
-    //     fechaFin:new Date("2022/11/10"),
-    //     lider:'61932bc8ea438642ed930ab7',
-    //     objetivos:["61933d2a8a6319eac3f1ade2"],
-    // }).then((u)=> {
-    //         console.log("proyecto creado",u)
-    // }).catch(e=>{
-    //         console.error("error creando el usuario, verifique el correo",e)
-    //     })
-//buscar proyect por nombre
-    // const proyecto = await projectModel.find({nombre:'proyecto1'}).populate("lider");
-    // // const proyecto = await projectModel.find({nombre:'proyecto1'}).populate({'lider'});
-    // console.log ("la info del lider es:",proyecto[0].lider)
-    // JSON.stringify(proyecto) lo convierte en texto plano
-
-
-// crear objetivo
-    // const object = await objetiveModel.create({
-    //     descripcion: "este es objetivo especifico1",
-    //     tipo : Enum_TipoObjetivo.especifico,
-    // }).then((u)=> {
-    //             console.log("proyecto creado",u)
-    //         }).catch(e=>{
-    //             console.error("error creando el usuario, verifique el correo",e)
-    //         })
+app.listen({port: process.env.PORT || 4000},async()=>{
+    await conectarBD();
+    await server.start();
+    server.applyMiddleware({app})
+    console.log("servidor listo")
+})
